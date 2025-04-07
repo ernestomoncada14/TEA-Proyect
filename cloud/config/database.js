12bb97b3718c9b4600e8a3a08d8013746e6fbaf4
ernestomoncada14 <ernesto.moncada@unah.hn>
2025-04-07 15:43:08 -0600

    nueva ruta

--- cloud/config/database.js
+++ cloud/config/database.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("sistema_agua", "root", "123", {
+const sequelize = new Sequelize("sistema_agua", "agua", "123456789", {
   host: "localhost",
   dialect: "mysql"
 });
